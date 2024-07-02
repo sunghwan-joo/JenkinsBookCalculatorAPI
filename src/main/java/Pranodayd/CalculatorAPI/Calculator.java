14fde593f970df15a7910380c34a2a3e10fa3c9f
sunghwan-joo <sunghwan.jooo@gmail.com>
2024-07-02 13:50:58 +0900

    Adding subtraction function in Java API project

--- src/main/java/Pranodayd/CalculatorAPI/Calculator.java
+++ src/main/java/Pranodayd/CalculatorAPI/Calculator.java
@@ -1,5 +1,8 @@
 package Pranodayd.CalculatorAPI;
 
 public class Calculator {
-
+	public int Subtraction(int num1, int num2) {
+		int Res = num1 - num2;
+		return Res;
+	}
 }
--- src/test/java/Pranodayd/CalculatorAPI/TestSubtractionFunctionality.java
+++ src/test/java/Pranodayd/CalculatorAPI/TestSubtractionFunctionality.java
@@ -1,5 +1,32 @@
 package Pranodayd.CalculatorAPI;
 
-public class TestSubtractionFunctionality {
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+public class TestSubtractionFunctionality {
+	Calculator Cal;
+	int Result;
+	@BeforeClass
+	public void Init() {
+		Cal = new Calculator();
+	}
+	
+	@BeforeMethod
+	public void ReInitialise() {
+		Result = 0;
+	}
+	
+	@Test(priority=1, groups= {"RegressionTest"})
+	public void TestSubtractionWithPositiveNumbers() {
+		Result = Cal.Subtraction(50, 10);
+		Assert.assertEquals(Result, 60, "Subtraction does not work with 1 Positive and 1 Negative Numbers");
+	}
+	
+	@AfterClass
+	public void Teardown() {
+		Cal = null;
+	}
 }
